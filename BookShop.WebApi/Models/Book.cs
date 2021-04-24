0f52ebbc4a408f05b17730943f824517bb58c30a
Alex <ffgh04@gmail.com>
2021-04-24 21:10:59 +0400

    Added models book and person

---
+++ BookShop.WebApi/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿namespace BookShop.WebApi.Models
+{
+    /// <summary>
+    /// 1.2 - 2
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// 1.2 - 2
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 1.2 - 2
+        /// </summary>
+        public Person Author { get; set; }
+
+        /// <summary>
+        /// 1.2 - 2
+        /// </summary>
+        public string Genre { get; set; }
+    }
+}
---
+++ BookShop.WebApi/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BookShop.WebApi.Models
+{
+    /// <summary>
+    /// 1.2 - 2
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// 1.2 - 2
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 1.2 - 2
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// 1.2 - 2
+        /// </summary>
+        public string Patronymic { get; set; }
+
+        /// <summary>
+        /// 1.2 - 2
+        /// </summary>
+        public DateTime BirthDate { get; set; }
+    }
+}
