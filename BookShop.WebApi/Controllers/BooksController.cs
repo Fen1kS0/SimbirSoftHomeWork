47b8fc36b89fa10812ab111ee2e6c4fb4ccf22df
Alex <ffgh04@gmail.com>
2021-04-24 22:55:42 +0400

    Added controllers for books and people

---
+++ BookShop.WebApi/Controllers/BooksController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BookShop.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.WebApi.Controllers
+{
+    /// <summary>
+    /// 1.2 - 3,
+    /// 1.2 - 4
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly MockDb _mockDb;
+
+        public BooksController(MockDb mockDb)
+        {
+            _mockDb = mockDb;
+        }
+
+        /// <summary>
+        /// 1.2 - 4.a
+        /// </summary>
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetAllBooks()
+        {
+            return Ok(_mockDb.Books);
+        }
+        
+        /// <summary>
+        /// 1.2 - 4.b
+        /// </summary>
+        [HttpGet("author/{authorId}")]
+        public ActionResult<IEnumerable<Book>> GetBooksByAuthor(Guid authorId)
+        {
+            return Ok(_mockDb.Books.Where(b => b.Author.Id == authorId));
+        }
+    }
+}
---
+++ BookShop.WebApi/Controllers/PeopleController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BookShop.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.WebApi.Controllers
+{
+    /// <summary>
+    /// 1.2 - 3,
+    /// 1.2 - 4
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        private readonly MockDb _mockDb;
+
+        public PeopleController(MockDb mockDb)
+        {
+            _mockDb = mockDb;
+        }
+
+        /// <summary>
+        /// 1.2 - 4.a
+        /// </summary>
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> GetAllPeople()
+        {
+            return Ok(_mockDb.People);
+        }
+        
+        /// <summary>
+        /// 1.2 - 4.c
+        /// </summary>
+        [HttpGet("name/{name}")]
+        public ActionResult<IEnumerable<Person>> GetAllPeopleByName(string name)
+        {
+            return Ok(_mockDb.People.Where(p => p.Name == name));
+        }
+    }
+}
