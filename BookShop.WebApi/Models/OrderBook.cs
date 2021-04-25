bb09e7c0b00a63d3c5a535cef61df0447817af68
Alex <ffgh04@gmail.com>
2021-04-25 10:32:36 +0400

    Created OrderBook.cs

---
+++ BookShop.WebApi/Models/OrderBook.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BookShop.WebApi.Models
+{
+    /// <summary>
+    /// 1.2.1 - 1
+    /// </summary>
+    public class OrderBook
+    {
+        public OrderBook()
+        {
+            Id = new Guid();
+        }
+        
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// 1.2.1 - 1
+        /// </summary>
+        public Book Book { get; set; }
+
+        /// <summary>
+        /// 1.2.1 - 1
+        /// </summary>
+        public Person Buyer { get; set; }
+
+        /// <summary>
+        /// 1.2.1 - 1
+        /// </summary>
+        public DateTime DateTimeOffset { get; set; }
+    }
+}
