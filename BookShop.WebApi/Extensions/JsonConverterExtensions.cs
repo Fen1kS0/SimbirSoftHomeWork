eafdf6fe17b264a80a13236e185dc25f013198d1
Alex <ffgh04@gmail.com>
2021-04-26 18:56:49 +0400

    Added DataTime and DataTimeOffset JsonConverter

---
+++ BookShop.WebApi/Extensions/JsonConverterExtensions.cs
@@ -0,0 +1,25 @@
+﻿using BookShop.WebApi.Convertors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookShop.WebApi.Extensions
+{
+    /// <summary>
+    /// 1.2.1 - 4
+    /// </summary>
+    public static class JsonConverterExtensions
+    {
+        /// <summary>
+        /// 1.2.1 - 4
+        /// </summary>
+        public static IServiceCollection AddJsonConverters(this IServiceCollection services)
+        {
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new DateTimeOffsetJsonConverter());
+                options.JsonSerializerOptions.Converters.Add(new DateTimeJsonConverter());
+            });
+
+            return services;
+        }
+    }
+}
