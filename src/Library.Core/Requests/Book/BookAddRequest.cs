bea293167e4f1c6def3d0b01053abf4105b43fad
ffgh04@gmail.com <Rango8525>
2021-05-14 22:14:19 +0400

    Added Book requests and response

---
+++ src/Library.Core/Requests/Book/BookAddRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Library.Core.Requests.Book
+{
+    public class BookAddRequest
+    {
+        public string Name { get; set; }
+
+        public Guid AuthorId { get; set; }
+    }
+}
