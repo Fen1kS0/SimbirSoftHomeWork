d908c76482a9c46a93d4a9202cde559fa340df9c
ffgh04@gmail.com <Rango8525>
2021-05-14 22:15:12 +0400

    Added Person requests and response

---
+++ src/Library.Core/Requests/Person/PeopleDeleteByFioRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Library.Core.Requests.Person
+{
+    public class PeopleDeleteByFioRequest
+    {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string MiddleName { get; set; }
+    }
+}
---
+++ src/Library.Core/Requests/Person/PersonUpdateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Library.Core.Requests.Person
+{
+    public class PersonUpdateRequest
+    {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string MiddleName { get; set; }
+        
+        public DateTime BirthDate { get; set; }
+    }
+}
