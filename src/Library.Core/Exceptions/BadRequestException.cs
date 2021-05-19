dda93b0f444c5869225b9a53d684554c477c0f53
Alex <ffgh04@gmail.com>
2021-05-19 22:24:51 +0400

    Created custom exceptions and ErrorMiddleware

---
+++ src/Library.Core/Exceptions/BadRequestException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Library.Core.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ src/Library.WebApi/Extensions/MiddlewareExtension.cs
@@ -0,0 +1,13 @@
+﻿using Library.WebApi.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace Library.WebApi.Extensions
+{
+    public static class MiddlewareExtension
+    {
+        public static IApplicationBuilder UseErrorMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ErrorMiddleware>();
+        }
+    }
+}
